cf122622a3c75b5e27cf5c02545be9f33626661c
Mio Nilsson <mio@ustwo.co.uk>
2013-11-25 11:49:01 +0000

    First version

---
+++ cexio-ticker.py
@@ -0,0 +1,50 @@
+import sublime
+import sublime_plugin
+
+try:
+  from urllib.request import urlopen
+  from urllib.parse import urlparse
+except ImportError:
+  from urlparse import urlparse
+  from urllib import urlopen
+
+import json
+import re
+
+class cexio-ticker(sublime_plugin.EventListener):
+
+  def update_status(self):
+    """
+      Updates the view's status bar with the current exchange rate
+    """
+
+    self.view.set_status('ghs', "GHS/BTC %.2f" % self.get_ticker())
+
+  def get_ticker(self):
+    """
+      Makes API call to cex.io to retrieve latest GHS rate.
+
+      Returns a the current exchange rate of 1 GHS in XBT.
+    """
+
+    settings = sublime.load_settings('cexio-tiker.sublime-settings')
+
+    url = 'https://cex.io/api/ticker/GHS/BTC'
+    req = urlparse(url)
+    resp = json.loads(urlopen(req.geturl()).read().decode('utf-8'))
+
+    ghs_in_btc = float(resp['last'])
+
+    return ghs_in_btc
+
+
+  def on_load(self, view):
+    self.view = view
+
+    settings = sublime.load_settings(__name__ + '.sublime-settings')
+    sublime.set_timeout(self.update_status, 10)
+
+  def on_post_save(self, view):
+    self.view = view
+
+    sublime.set_timeout(self.update_status, 10)
